cc72f9ebaf935e36ca242eb36918fbd1c462b796
orion <oriogui@terpmail.umd.edu>
2023-10-26 18:02:57 -0400

    initial update of astrom related files, taken almost directly from my scamptractor.py

--- photomitrus/astrometry/astrometry.py
+++ photomitrus/astrometry/astrometry.py
@@ -1,3 +1,47 @@
 """
 Runs improved astrometry on a given file using sextractor and scamp
 """
+#%%
+import os
+import argparse
+
+def sex(imgdir, outdir):
+    os.chdir(str(outdir))
+    for f in os.listdir(str(imgdir)):
+        if f.endswith('.fits') or f.endswith('.new'):
+            pre = os.path.splitext(f)[0]
+            ext = os.path.splitext(f)[1]
+            com = ["sex ", imgdir + pre + ext, ' -c /Users/orion/Desktop/PRIME/sex.config',
+                   " -CATALOG_NAME " + pre + '.fits']  # change url for config file
+            s0 = ''
+            com = s0.join(com)
+            out = subprocess.Popen([com], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            out.wait()
+            print(pre + ext + ' sextracted!')
+
+def scamp(imgdir, outdir):
+    for f in os.listdir(str(outdir)):  # loop to generate scamp .head headers
+        if f.endswith('.fits'):
+            pre = os.path.splitext(f)[0]
+            ext = os.path.splitext(f)[1]
+            com = ["scamp ", outdir + pre + ext, ' -c /Users/orion/Desktop/PRIME/scamp.conf']
+            s0 = ''
+            com = s0.join(com)
+            out = subprocess.Popen([com], shell=True,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            out.wait()
+            print(pre + ext + ' scamped!')
+    for f in os.listdir(str(outdir)):  # loop to move .heads to img dir, so both are in same dir for swarp to stack, remove if you don't want that
+        if f.endswith('.head'):
+            pre1 = os.path.splitext(f)[0]
+            ext1 = os.path.splitext(f)[1]
+            os.rename(outdir + pre1 + ext1, imgdir + pre1 + ext1)
+            print(pre1 + ext1 + ' moved!')
+
+#%%
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='runs sextractor on input imgs, runs scamp, then moves scamp ext. hdrs '
+                                                 'to img dir.; generates .head and fits LDAC files')
+    parser.add_argument('files', nargs='*', type=str, metavar='f', help='Put input file first and output path last')
+    args = parser.parse_args()
+    sex(args.files[0],args.files[1])
+    scamp(args.files[0],args.files[1])
--- photomitrus/stack/stack.py
+++ photomitrus/stack/stack.py
@@ -1,3 +1,22 @@
 """
-Stacks astrometrically calibrated files using scamp
+Stacks astrometrically calibrated files using swarp
 """
+#%%
+import os
+import argparse
+
+def swarp(indir, finout):
+    os.chdir(str(finout))
+    com = ["swarp ", indir + '*.new', ' -c /Users/orion/Desktop/PRIME/default.swarp'] #change config file path, also
+    s0 = ''
+    com = s0.join(com)
+    out = subprocess.Popen([com], shell=True)
+    out.wait()
+    print('Co-added image created, all done!')
+
+#%%
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Runs swarp to stack imgs')
+    parser.add_argument('files', nargs='*', type=str, metavar='f', help='Put input file first and output path last')
+    args = parser.parse_args()
+    swarp(args.files[0],args.files[1])
