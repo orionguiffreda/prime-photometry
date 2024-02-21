a85cfbd2737067f0c29afd6c91b2c3c2444ed17f
joedurbak <durbak.3@gmail.com>
2024-02-21 23:37:01 +0100

    major photometry.py improvements: included improved box region query; added VISTA VHS survey for calibration; added optional args to export survey query results and export mag comparison and residual plots

--- photomitrus/photometry/photometry.py
+++ photomitrus/photometry/photometry.py
@@ -4,6 +4,7 @@ Calibrates photometry for stacked image
 
 import numpy as np
 import numpy.ma as ma
+import warnings
 import argparse
 import sys
 import astropy.units as u
@@ -12,8 +13,12 @@ from astropy.wcs import WCS
 from astropy.stats import sigma_clipped_stats, sigma_clip
 import subprocess
 from astropy.io import fits
+from astropy.io import ascii
 import os
 from astropy.table import Column
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from scipy import stats
 
 sys.path.insert(0,'C:\PycharmProjects\prime-photometry\photomitrus')
 from settings import gen_config_file_name
@@ -53,38 +58,70 @@ def img(directory,imageName,crop):
     w = WCS(header)
 
     #Get the RA and Dec of the center of the image
-    raImage, decImage = w.all_pix2world(data.shape[0]/2, data.shape[1]/2, 1)
+    [raImage, decImage] = w.all_pix2world(data.shape[0] / 2, data.shape[1] / 2, 1)
 
-    #get ra and dec of edge, subtract to get rough rad of image
-    [raEdge, decEdge] = w.all_pix2world(data.shape[0]-int(crop), data.shape[1] / 2, 1)
-    boxsize = (raImage - raEdge)*60 #rad in arcmin
+    #get ra and dec of right, left, top and bottom of img for box size
+    [raREdge, decREdge] = w.all_pix2world(data.shape[0] - int(crop), data.shape[1] / 2, 1)
+    [raLEdge, decLEdge] = w.all_pix2world(int(crop), data.shape[1] / 2, 1)
+    [raTop, decTop] = w.all_pix2world(data.shape[0] / 2, data.shape[1] - int(crop), 1)
+    [raBot, decBot] = w.all_pix2world(data.shape[0] / 2, int(crop), 1)
 
-    return data,header,w,raImage, decImage, boxsize
+    height = (decTop - decBot) * 60
+    width = (raLEdge - raREdge) * 60
 
-#data,header,w,raImage, decImage = img(directory,imageName)
+    return data,header,w,raImage, decImage, width, height
+
+#data,header,w,raImage, decImage,boxsize = img('/mnt/d/PRIME_photometry_test_files/GRB_Followup/GRB_Followup_flat_tests/flatstack/','coadd.Open-J.00888557-00888869.C4.fits',300)
 #%%
 # Use astroquery to get catalog search
 from astroquery.vizier import Vizier
 #Vizier.VIZIER_SERVER = 'vizier.ast.cam.ac.uk'
 
-def query(raImage, decImage,boxsize):
-    #boxsize = 33.4  # Set the box size to search for catalog stars
-    maxmag = 18     # Magnitude cut-offs of sources to be cross-matched against
-    catNum = 'II/246'  # changing to 2mass
-    print('\nQuerying Vizier %s around RA %.4f, Dec %.4f with a radius of %.4f arcmin' % (
-    catNum, raImage, decImage, boxsize))
-    try:
-        #You can set the filters for the individual columns (magnitude range, number of detections) inside the Vizier query
-        v = Vizier(columns=['*'], column_filters={"gmag":"<%.2f"%maxmag, "Nd":">6", "e_gmag":"<1.086/3"}, row_limit=-1)
-        Q = v.query_region(SkyCoord(ra = raImage, dec = decImage, unit = (u.deg, u.deg)), radius = str(boxsize)+'m', catalog=catNum, cache=False)
-        #query vizier around (ra, dec) with a radius of boxsize
-        #print(Q[0])
-        print(Q[0].colnames)
-    except:
-        print('I cannnot reach the Vizier database. Is the internet working?')
+def query(raImage, decImage,filter, width, height, survey):
+    # Use astroquery to get catalog search
+    from astroquery.vizier import Vizier
+    # Vizier.VIZIER_SERVER = 'vizier.ast.cam.ac.uk'
+    if survey == '2MASS':
+        catNum = 'II/246'  # changing to 2mass
+        print('\nQuerying Vizier %s around RA %.4f, Dec %.4f with a box of width %.3f and height %.3f arcmin' % (
+        catNum, raImage, decImage, width, height))
+        try:
+            # You can set the filters for the individual columns (magnitude range, number of detections) inside the Vizier query
+            v = Vizier(columns=['*'], column_filters={"%smag" % filter: ">12", "Nd": ">6"}, row_limit=-1)
+            Q = v.query_region(SkyCoord(ra=raImage, dec=decImage, unit=(u.deg, u.deg)), width=str(width) + 'm',
+                               height=str(height) + 'm', catalog=catNum, cache=False)
+            # query vizier around (ra, dec) with a radius of boxsize
+            #print(Q[0])
+            print('Queried source total = ', len(Q[0]))
+        except:
+            print('I cannnot reach the Vizier database. Is the internet working?')
+    elif survey == 'VHS':
+        catNum = 'II/367'  # changing to vista
+        print('\nQuerying Vizier %s around RA %.4f, Dec %.4f with a box of width %.3f and height %.3f arcmin' % (
+        catNum, raImage, decImage, width, height))
+        try:
+            # You can set the filters for the individual columns (magnitude range, number of detections) inside the Vizier query
+            v = Vizier(columns=['RAJ2000','DEJ2000','%sap3' % filter,'e_%sap3' % filter], column_filters={"%sap3" % filter: ">12"}, row_limit=-1)
+            Q = v.query_region(SkyCoord(ra=raImage, dec=decImage, unit=(u.deg, u.deg)), width=str(width) + 'm',
+                               height=str(height) + 'm', catalog=catNum, cache=False)
+            # query vizier around (ra, dec) with a radius of boxsize
+            #print(Q[0])
+            print('Queried source total = ', len(Q[0]))
+        except:
+            print(
+                'Error in Vizier query. Perhaps your image is not in the southern hemisphere sky?')
+    else:
+        print('No supported survey found, currently use either 2MASS or VHS')
     return Q
 
-#Q = query(catNum, raImage, decImage)
+def queryexport(Q, survey):
+    table = Q[0]
+    table.write('%s_query.ecsv' % survey, overwrite=True)
+    print('%s_query.ecsv written!' % survey)
+
+#Q = query(raImage, decImage, boxsize)
+#table = Q[0]
+#table.write('2MASS_query.ecsv', overwrite=True)
 #%%
 #run sextractor on swarped img to find sources
 """configFile = '/Users/orion/Desktop/PRIME/sex.config'
@@ -97,7 +134,7 @@ def sex1(imageName):
     catalogName = imageName + '.cat'
     try:
         command = 'sex %s -c %s -CATALOG_NAME %s -PARAMETERS_NAME %s' % (imageName, configFile, catalogName, paramName)
-        print('Executing command: %s' % command)
+        #print('Executing command: %s' % command)
         rval = subprocess.run(command.split(), check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     except subprocess.CalledProcessError as err:
         print('Could not run sextractor with exit error %s'%err)
@@ -110,7 +147,7 @@ def psfex(catalogName):
     psfConfigFile = gen_config_file_name('default.psfex')
     try:
         command = 'psfex %s -c %s' % (catalogName,psfConfigFile)
-        print('Executing command: %s' % command)
+        #print('Executing command: %s' % command)
         rval = subprocess.run(command.split(), check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     except subprocess.CalledProcessError as err:
         print('Could not run psfex with exit error %s'%err)
@@ -129,7 +166,7 @@ def sex2(imageName):
     try:
         #We are supplying SExtactor with the PSF model with the PSF_NAME option
         command = 'sex %s -c %s -CATALOG_NAME %s -PSF_NAME %s -PARAMETERS_NAME %s' % (imageName, configFile, psfcatalogName, psfName, psfparamName)
-        print("Executing command: %s" % command)
+        #print("Executing command: %s" % command)
         rval = subprocess.run(command.split(), check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     except subprocess.CalledProcessError as err:
         print('Could not run sextractor with exit error %s'%err)
@@ -163,8 +200,15 @@ def tables(Q,psfcatalogName,crop):
 #good_cat_stars,cleanPSFSources,idx_psfmass,idx_psfimage = tables(Q,psfcatalogName)
 #%%
 #derive zero pt / put in swarped header
-def zeropt(good_cat_stars,cleanPSFSources,idx_psfmass,idx_psfimage,imageName,filter):
-    psfoffsets = ma.array(good_cat_stars['%smag' % filter][idx_psfmass] - cleanPSFSources['MAG_POINTSOURCE'][idx_psfimage]) # *TO WORK ON* currently need to change the filter here manually
+def zeropt(good_cat_stars,cleanPSFSources,idx_psfmass,idx_psfimage,imageName,filter,survey):
+    if survey == '2MASS':
+        psfoffsets = ma.array(good_cat_stars['%smag' % filter][idx_psfmass] - cleanPSFSources['MAG_POINTSOURCE'][idx_psfimage])
+        psfoffsets = psfoffsets.data
+    elif survey == 'VHS':
+        psfoffsets = ma.array(good_cat_stars['%sap3' % filter][idx_psfmass] - cleanPSFSources['MAG_POINTSOURCE'][idx_psfimage])
+        psfoffsets = psfoffsets.data
+    else:
+        print('Surveys other than 2MASS or VHS currently not supported')
     #Compute sigma clipped statistics
     zero_psfmean, zero_psfmed, zero_psfstd = sigma_clipped_stats(psfoffsets)
     print('PSF Mean ZP: %.2f\nPSF Median ZP: %.2f\nPSF STD ZP: %.2f'%(zero_psfmean, zero_psfmed, zero_psfstd))
@@ -187,28 +231,119 @@ def zeropt(good_cat_stars,cleanPSFSources,idx_psfmass,idx_psfimage,imageName,fil
     cleanPSFSources.add_column(psfmagerrcol)
     cleanPSFSources.remove_column('VIGNET')
 
-    cleanPSFSources.write('%s.mag.ecsv' % imageName,overwrite=True)
-    print('%s.mag.ecsv written, CSV w/ corrected mags' % imageName)
+    cleanPSFSources.write('%s.%s.ecsv' % (imageName,survey),overwrite=True)
+    print('%s.%s.ecsv written, CSV w/ corrected mags' % (imageName,survey))
+
+#%% optional plots
+def plots(directory,imageName,survey,filter,good_cat_stars,idx_psfmass,idx_psfimage):
+    #appropriate mag column
+    if survey == '2MASS':
+        magcol = '%smag' % filter
+    elif survey == 'VHS':
+        magcol = '%sap3' % filter
+    else:
+        print('Surveys other than 2MASS or VHS currently not supported')
+
+    # mag comparison plot
+    PRIMEdata = ascii.read(directory + imageName + '.' + survey + '.ecsv')
+    plt.figure(1,figsize=(8, 8))
+    plt.plot(PRIMEdata['%sMAG_PSF' % filter][idx_psfimage],good_cat_stars['%s' % magcol][idx_psfmass],
+             'r.', markersize=14, markeredgecolor='black',)
+    plt.xlim(10, 22)
+    plt.ylim(10, 22)
+    plt.title('PRIME Mags vs %s Mags' % survey)
+    plt.xlabel('PRIME Mags', fontsize=15)
+    plt.ylabel('%s Mags' % survey, fontsize=15)
+    plt.grid()
+    plt.savefig('%s_mag_comp_plot.png' % survey)
+    print('Saved mag comparison plot to dir!')
+
+    #residual fit
+    x = PRIMEdata['%sMAG_PSF' % filter][idx_psfimage]
+    y = good_cat_stars['%s' % magcol][idx_psfmass]
+    x_const = sm.add_constant(x)
+    model = sm.OLS(y, x_const).fit()
+    m = model.params[1]
+    merr = model.bse[1]
+    b = model.params[0]
+    berr = model.bse[0]
+    rsquare = model.rsquared
+    rss = model.ssr
+    res_err = np.std(model.resid)
+
+    #residual plot
+    plt.figure(2, figsize=(8, 6))
+    plt.scatter(PRIMEdata['%sMAG_PSF' % filter][idx_psfimage], model.resid, color='red')
+    plt.ylim(-1.5, 1.5)
+    plt.title('PRIME vs %s Residuals' % survey)
+    plt.ylabel('Residuals')
+    plt.xlabel('Mags')
+    plt.axhline(y=res_err, color='tab:orange', linestyle='--', linewidth=1)
+    plt.axhline(y=res_err * 2, color='green', linestyle='--', linewidth=1)
+    plt.axhline(y=-res_err, color='tab:orange', linestyle='--', linewidth=1)
+    plt.axhline(y=-res_err * 2, color='green', linestyle='--', linewidth=1)
+    plt.axhline(y=0, color='black', linestyle='--', linewidth=1)
+    plt.legend(['Residuals', r'1 $\sigma$ = %.3f' % res_err, r'2 $\sigma$'], loc='lower left')
+    info = ('slope = %.3f +/- %.3f' % (m,merr))+('\nintercept = %.3f +/- %.3f' % (b,berr))+('\nR$^{2}$ = %.3f' % rsquare)+('\nRSS = %d' % rss)
+    plt.text(15,-1.25,info,bbox=dict(facecolor='none',edgecolor='black',pad=5.0))
+    plt.savefig('%s_residual_plot.png' % survey,dpi=300)
+    print('Saved residual plot to dir!')
+
 
 #%%
-defaults = dict(crop=300)
+defaults = dict(crop=300,survey='2MASS')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='runs sextractor and psfex on swarped img to get psf fit photometry, then '
-                                                 'writes zero pts to img header and outputs csv w/ corrected mags')
+                                                 'outputs ecsv w/ corrected mags')
+    parser.add_argument('-exp_query', action='store_true', help='optional flag, exports ecsv of astroquery results '
+                                                                'along with photometry')
+    parser.add_argument('-plots', action='store_true', help='optional flag, exports mag comparison plot betw. PRIME and survey, '
+                                                            'along with residual plot w/ statistics')
     parser.add_argument('-dir', type=str, help='[str], directory of stacked image')
     parser.add_argument('-name', type=str, help='[str], image name')
     parser.add_argument('-filter', type=str, help='[str], filter, ex. "J"')
-    parser.add_argument('-crop', type=str, help='[int], # of pixels from edge of image to crop, default 300',default=defaults["crop"])
+    parser.add_argument('-survey', type=str, help='[str], survey to query, currently use either "2MASS" '
+                                                  'or "VHS" (for VISTA in s. hemi. only), default = 2MASS')
+    parser.add_argument('-crop', type=str, help='[int], # of pixels from edge of image to crop, default = 300',default=defaults["crop"])
     args = parser.parse_args()
 
-    data, header, w, raImage, decImage, boxsize = img(args.dir, args.name, args.crop)
-    Q = query(raImage, decImage, boxsize)
-    catalogName = sex1(args.name)
-    psfex(catalogName)
-    psfcatalogName = sex2(args.name)
-    good_cat_stars, cleanPSFSources, idx_psfmass, idx_psfimage = tables(Q, psfcatalogName, args.crop)
-    zeropt(good_cat_stars, cleanPSFSources, idx_psfmass, idx_psfimage, args.name, args.filter)
+    if args.exp_query and args.plots:
+        data, header, w, raImage, decImage, width, height = img(args.dir, args.name, args.crop)
+        Q = query(raImage, decImage, args.filter, width, height, args.survey)
+        queryexport(Q,args.survey)
+        catalogName = sex1(args.name)
+        psfex(catalogName)
+        psfcatalogName = sex2(args.name)
+        good_cat_stars, cleanPSFSources, idx_psfmass, idx_psfimage = tables(Q, psfcatalogName, args.crop)
+        zeropt(good_cat_stars, cleanPSFSources, idx_psfmass, idx_psfimage, args.name, args.filter, args.survey)
+        plots(args.dir, args.name, args.survey, args.filter, good_cat_stars, idx_psfmass, idx_psfimage)
+    elif args.exp_query:
+        data, header, w, raImage, decImage, width, height = img(args.dir, args.name, args.crop)
+        Q = query(raImage, decImage, args.filter, width, height, args.survey)
+        queryexport(Q,args.survey)
+        catalogName = sex1(args.name)
+        psfex(catalogName)
+        psfcatalogName = sex2(args.name)
+        good_cat_stars, cleanPSFSources, idx_psfmass, idx_psfimage = tables(Q, psfcatalogName, args.crop)
+        zeropt(good_cat_stars, cleanPSFSources, idx_psfmass, idx_psfimage, args.name, args.filter, args.survey)
+    elif args.plots:
+        data, header, w, raImage, decImage, width, height = img(args.dir, args.name, args.crop)
+        Q = query(raImage, decImage, args.filter, width, height, args.survey)
+        catalogName = sex1(args.name)
+        psfex(catalogName)
+        psfcatalogName = sex2(args.name)
+        good_cat_stars, cleanPSFSources, idx_psfmass, idx_psfimage = tables(Q, psfcatalogName, args.crop)
+        zeropt(good_cat_stars, cleanPSFSources, idx_psfmass, idx_psfimage, args.name, args.filter,args.survey)
+        plots(args.dir,args.name,args.survey,args.filter,good_cat_stars,idx_psfmass,idx_psfimage)
+    else:
+        data, header, w, raImage, decImage, width, height = img(args.dir, args.name, args.crop)
+        Q = query(raImage, decImage, args.filter, width, height, args.survey)
+        catalogName = sex1(args.name)
+        psfex(catalogName)
+        psfcatalogName = sex2(args.name)
+        good_cat_stars, cleanPSFSources, idx_psfmass, idx_psfimage = tables(Q, psfcatalogName, args.crop)
+        zeropt(good_cat_stars, cleanPSFSources, idx_psfmass, idx_psfimage, args.name, args.filter,args.survey)
 
 #%%
 
