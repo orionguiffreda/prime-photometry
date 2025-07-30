d843c04a8ca971865bd3f6771065664069c904f8
gsfcprime <joseph.m.durbak@nasa.gov>
2025-07-30 17:04:56 -0400

    added cli to conda bin

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 conda create -n prime-photometrus numpy scikit-image scipy pandas astropy statsmodels ipython matplotlib astroquery astromatic-psfex astromatic-scamp astromatic-source-extractor astromatic-swarp astrometry pysftp json5 conda-build -c conda-forge
 python build.py
 conda develop .
+ln -s bin/photometrus $CONDA_PREFIX/bin/photometrus
